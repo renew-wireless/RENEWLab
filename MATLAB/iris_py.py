ad9180514a3811c08526903b22fac520527e6746
rdoost <doost@rice.edu>
2019-07-31 14:46:43 -0500

    updated framer settings, IQ order for matlab py script
    Former-commit-id: 1041dec2281a09d3707605e08b093d69746f3be9

--- MATLAB/iris_py.py
+++ MATLAB/iris_py.py
@@ -30,6 +30,7 @@ import json
 import threading
 from generate_sequence import *
 import matplotlib.pyplot as plt
+from type_conv import *
 
 
 # CORRTHRESHOLDING REGS
@@ -55,76 +56,8 @@ FPGA_IRIS030_WR_PKT_DET_NEW_FRAME = 300
 tx_advance = 68
 corr_threshold = 1
 
-#Beacon:
-beacon = np.array([-0.56463587-0.56463587j ,0.46699509+0.34623110j ,0.87693578-0.35418823j ,0.54079849+0.34757638j ,-0.48144832+0.00891686j ,-0.88476783-0.00866229j,
-		0.33639774+0.37122476j ,-0.43609348-0.42793053j ,-0.26278743+0.41920760j ,0.69103312+0.17364220j ,-0.25535262+0.24096492j ,0.11774132+0.22344504j,
-		0.46892625-0.37271422j ,0.77644444+0.42836890j ,-0.14834122+0.40922493j ,-0.13464923+0.03079897j ,-0.26617187+0.53234375j ,0.13412920+0.73846632j,
-		0.13357399+0.19951832j ,0.15594807-0.13213991j ,-0.05784707+0.18199626j ,0.39676210+0.38991654j ,0.04760613+0.22615194j ,0.01414329-0.40837204j,
-		0.41560003+0.33933204j ,0.12632199-0.68115342j ,-0.33603117-0.12019706j ,-0.56691819-0.40438867j ,-0.20043480-0.18971694j ,0.55602646+0.00865902j,
-		0.24340886+0.11038142j ,-0.16611233-0.90827960j ,0.79049021-0.18821196j ,-0.42025912+0.31633607j ,-0.38651145+0.92221844j ,-0.14808364+0.82918876j,
-		-0.27662534-0.25830117j ,-0.74715126+0.01612155j ,0.59089267-0.14366682j ,-0.75451213+0.18401834j ,-0.33933204-0.23919682j ,0.36646086+0.42271641j,
-		-0.57852495-0.06262089j ,0.10015667+0.35279569j ,-0.34719938+0.64131898j ,0.35134000-0.82117832j ,0.73830807-0.38437498j ,-0.37431011-0.33556038j,
-		-0.53234375+0.26617187j ,-0.33714586-0.00221796j ,0.01215767-0.44250035j ,-0.39932099-0.15740044j ,-0.38716090-0.72959810j ,0.27705255+0.26336509j,
-		0.44698530+0.25250778j ,-0.16857521-0.08888090j ,0.60894567+0.26278743j ,-0.04652265+0.64291787j ,0.21421923+0.32680589j ,0.01422996+0.19918670j,
-		0.87569416-0.08036269j ,-0.28046992-0.09114221j ,0.64841086+0.68264091j ,0.06317055-0.73030668j ,-0.03764239-0.03764239j ,-0.73030668+0.06317055j,
-		0.68264091+0.64841086j ,-0.09114221-0.28046992j ,-0.08036269+0.87569416j ,0.19918670+0.01422996j ,0.32680589+0.21421923j ,0.64291787-0.04652265j,
-		0.26278743+0.60894567j ,-0.08888090-0.16857521j ,0.25250778+0.44698530j ,0.26336509+0.27705255j ,-0.72959810-0.38716090j ,-0.15740044-0.39932099j,
-		-0.44250035+0.01215767j ,-0.00221796-0.33714586j ,0.26617187-0.53234375j ,-0.33556038-0.37431011j ,-0.38437498+0.73830807j ,-0.82117832+0.35134000j,
-		0.64131898-0.34719938j ,0.35279569+0.10015667j ,-0.06262089-0.57852495j ,0.42271641+0.36646086j ,-0.23919682-0.33933204j ,0.18401834-0.75451213j,
-		-0.14366682+0.59089267j ,0.01612155-0.74715126j ,-0.25830117-0.27662534j ,0.82918876-0.14808364j ,0.92221844-0.38651145j ,0.31633607-0.42025912j,
-		-0.18821196+0.79049021j ,-0.90827960-0.16611233j ,0.11038142+0.24340886j ,0.00865902+0.55602646j ,-0.18971694-0.20043480j ,-0.40438867-0.56691819j,
-		-0.12019706-0.33603117j ,-0.68115342+0.12632199j ,0.33933204+0.41560003j ,-0.40837204+0.01414329j ,0.22615194+0.04760613j ,0.38991654+0.39676210j,
-		0.18199626-0.05784707j ,-0.13213991+0.15594807j ,0.19951832+0.13357399j ,0.73846632+0.13412920j ,0.53234375-0.26617187j ,0.03079897-0.13464923j,
-		0.40922493-0.14834122j ,0.42836890+0.77644444j ,-0.37271422+0.46892625j ,0.22344504+0.11774132j ,0.24096492-0.25535262j ,0.17364220+0.69103312j,
-		0.41920760-0.26278743j ,-0.42793053-0.43609348j ,0.37122476+0.33639774j ,-0.00866229-0.88476783j ,0.00891686-0.48144832j ,0.34757638+0.54079849j,
-		-0.35418823+0.87693578j ,0.34623110+0.46699509j ,-0.56463587-0.56463587j ,0.46699509+0.34623110j ,0.87693578-0.35418823j ,0.54079849+0.34757638j,
-		-0.48144832+0.00891686j ,-0.88476783-0.00866229j ,0.33639774+0.37122476j ,-0.43609348-0.42793053j ,-0.26278743+0.41920760j ,0.69103312+0.17364220j,
-		-0.25535262+0.24096492j ,0.11774132+0.22344504j ,0.46892625-0.37271422j ,0.77644444+0.42836890j ,-0.14834122+0.40922493j ,-0.13464923+0.03079897j,
-		-0.26617187+0.53234375j ,0.13412920+0.73846632j ,0.13357399+0.19951832j ,0.15594807-0.13213991j ,-0.05784707+0.18199626j ,0.39676210+0.38991654j,
-		0.04760613+0.22615194j ,0.01414329-0.40837204j ,0.41560003+0.33933204j ,0.12632199-0.68115342j ,-0.33603117-0.12019706j ,-0.56691819-0.40438867j,
-		-0.20043480-0.18971694j ,0.55602646+0.00865902j ,0.24340886+0.11038142j ,-0.16611233-0.90827960j ,0.79049021-0.18821196j ,-0.42025912+0.31633607j,
-		-0.38651145+0.92221844j ,-0.14808364+0.82918876j ,-0.27662534-0.25830117j ,-0.74715126+0.01612155j ,0.59089267-0.14366682j ,-0.75451213+0.18401834j,
-		-0.33933204-0.23919682j ,0.36646086+0.42271641j ,-0.57852495-0.06262089j ,0.10015667+0.35279569j ,-0.34719938+0.64131898j ,0.35134000-0.82117832j,
-		0.73830807-0.38437498j ,-0.37431011-0.33556038j ,-0.53234375+0.26617187j ,-0.33714586-0.00221796j ,0.01215767-0.44250035j ,-0.39932099-0.15740044j,
-		-0.38716090-0.72959810j ,0.27705255+0.26336509j ,0.44698530+0.25250778j ,-0.16857521-0.08888090j ,0.60894567+0.26278743j ,-0.04652265+0.64291787j,
-		0.21421923+0.32680589j ,0.01422996+0.19918670j ,0.87569416-0.08036269j ,-0.28046992-0.09114221j ,0.64841086+0.68264091j ,0.06317055-0.73030668j,
-		-0.03764239-0.03764239j ,-0.73030668+0.06317055j ,0.68264091+0.64841086j ,-0.09114221-0.28046992j ,-0.08036269+0.87569416j ,0.19918670+0.01422996j,
-		0.32680589+0.21421923j ,0.64291787-0.04652265j ,0.26278743+0.60894567j ,-0.08888090-0.16857521j ,0.25250778+0.44698530j ,0.26336509+0.27705255j,
-		-0.72959810-0.38716090j ,-0.15740044-0.39932099j ,-0.44250035+0.01215767j ,-0.00221796-0.33714586j ,0.26617187-0.53234375j ,-0.33556038-0.37431011j,
-		-0.38437498+0.73830807j ,-0.82117832+0.35134000j ,0.64131898-0.34719938j ,0.35279569+0.10015667j ,-0.06262089-0.57852495j ,0.42271641+0.36646086j,
-		-0.23919682-0.33933204j ,0.18401834-0.75451213j ,-0.14366682+0.59089267j ,0.01612155-0.74715126j ,-0.25830117-0.27662534j ,0.82918876-0.14808364j,
-		0.92221844-0.38651145j ,0.31633607-0.42025912j ,-0.18821196+0.79049021j ,-0.90827960-0.16611233j ,0.11038142+0.24340886j ,0.00865902+0.55602646j,
-		-0.18971694-0.20043480j ,-0.40438867-0.56691819j ,-0.12019706-0.33603117j ,-0.68115342+0.12632199j ,0.33933204+0.41560003j ,-0.40837204+0.01414329j,
-		0.22615194+0.04760613j ,0.38991654+0.39676210j ,0.18199626-0.05784707j ,-0.13213991+0.15594807j ,0.19951832+0.13357399j ,0.73846632+0.13412920j,
-		0.53234375-0.26617187j ,0.03079897-0.13464923j ,0.40922493-0.14834122j ,0.42836890+0.77644444j ,-0.37271422+0.46892625j ,0.22344504+0.11774132j,
-		0.24096492-0.25535262j ,0.17364220+0.69103312j ,0.41920760-0.26278743j ,-0.42793053-0.43609348j , 0.37122476+0.33639774j ,-0.00866229-0.88476783j,
-		0.00891686-0.48144832j ,0.34757638+0.54079849j ,-0.35418823+0.87693578j ,0.34623110+0.46699509j], dtype=np.complex64)
-
-
 #######################################				
-#######		Help functions:		#######
-#######################################
-
-
-def cfloat2uint32(arr, order='IQ'):
-		arr_i = (np.real(arr) * 32767).astype(np.uint16)
-		arr_q = (np.imag(arr) * 32767).astype(np.uint16)
-		if order == 'IQ':
-			return np.bitwise_or(arr_q ,np.left_shift(arr_i.astype(np.uint32), 16))
-		else:
-			return np.bitwise_or(arr_i ,np.left_shift(arr_q.astype(np.uint32), 16))
-	
-def uint32tocfloat(arr, order='IQ'):
-	arr_hi = ((np.right_shift(arr, 16).astype(np.int16)) /32768.0)
-	arr_lo = (np.bitwise_and(arr, 0xFFFF).astype(np.int16)) /32768.0
-	if order == 'IQ':
-		return (arr_hi + 1j*arr_lo).astype(np.complex64)
-	else:
-		return (arr_lo + 1j*arr_hi).astype(np.complex64)
-
-
-#######################################				
-#######			SDR Class:		#######
+#######	SDR Class:		#######
 #######################################
 
 class Iris_py:
@@ -181,7 +114,7 @@ class Iris_py:
 			
 			print("Set TX frequency to %f" % self.sdr.getFrequency(SOAPY_SDR_TX, chan))
 			#self.sdr.setAntenna(SOAPY_SDR_TX, chan, "TRX")
-			self.sdr.setGain(SOAPY_SDR_TX, chan, 'IAMP', 12) #[0,12]
+			self.sdr.setGain(SOAPY_SDR_TX, chan, 'IAMP', 0) #[0,12]
 			self.sdr.setGain(SOAPY_SDR_TX, chan, 'PAD', tx_gain) #[-52,0]
 			
 			#Rx:
@@ -247,12 +180,12 @@ class Iris_py:
 			self.tdd_sched = tdd_sched
 		else: self.tdd_sched = "G"
 		print(tdd_sched)
-		conf_str = {"tdd_enabled": True, "trigger_out": True, "wait_trigger": True, "symbol_size" : self.n_samp, "max_frame": 1, "frames": [self.tdd_sched]}
+		conf_str = {"tdd_enabled": True, "frame_mode": "free_running", "symbol_size" : self.n_samp, "max_frame": 1, "frames": [self.tdd_sched]}
 		self.sdr.writeSetting("TDD_CONFIG", json.dumps(conf_str))
 		self.sdr.writeSetting("TDD_MODE", "true")
 		self.sdr.writeSetting("TX_SW_DELAY", str(30))
 
-	def config_sdr_tdd(self, tdd_sched = None, is_bs = True, trigger_out = False):
+	def config_sdr_tdd(self, tdd_sched = None, is_bs = True):
 		'''Configure the TDD schedule and functionality when unchained. Set up the correlator.'''
 		global tx_advance, corr_threshold, beacon
 		len_beacon_zpad = len(beacon) + self.n_zpad_samp
@@ -263,13 +196,11 @@ class Iris_py:
 		print(tdd_sched)
 		
 		if bool(is_bs):
-			conf_str = {"tdd_enabled": True, "trigger_out": False, "symbol_size" : self.n_samp, "max_frame": 0, "frames": [self.tdd_sched]}
+			conf_str = {"tdd_enabled": True, "frame_mode": "free_running", "symbol_size" : self.n_samp, "max_frame": 1, "frames": [self.tdd_sched]}
 			self.sdr.writeSetting("TDD_CONFIG", json.dumps(conf_str))
-			self.sdr.writeSetting("TX_SW_DELAY", str(30))
 		else:
-			conf_str = {"tdd_enabled": True, "trigger_out": not trigger_out, "wait_trigger": True, "symbol_size" : self.n_samp, "frames": [self.tdd_sched]}
+			conf_str = {"tdd_enabled": True, "frame_mode": "triggered", "symbol_size" : self.n_samp, "frames": [self.tdd_sched]}
 			self.sdr.writeSetting("TDD_CONFIG", json.dumps(conf_str))
-			self.sdr.writeSetting("TX_SW_DELAY", str(30))
 			
 			#Correlator stuff:
 			self.sdr.writeRegister("IRIS30", CORR_CONF, int("00004001", 16)) # enable the correlator, with zeros as inputs 
@@ -292,6 +223,7 @@ class Iris_py:
 			print("config_sdr_tdd: UE starting symbol and sample count (%d, %d)" % (sf_start, sp_start))
 			self.sdr.setHardwareTime(SoapySDR.ticksToTimeNs((sf_start<<16) | sp_start, self.sample_rate),"TRIGGER") # make sure to set this after TDD mode is enabled "writeSetting("TDD_CONFIG", ..."
 		
+		self.sdr.writeSetting("TX_SW_DELAY", str(30))
 		self.sdr.writeSetting("TDD_MODE", "true")
 
 
@@ -311,7 +243,7 @@ class Iris_py:
     # Setup and activate RX streams
 	def setup_stream_rx(self):
 		print("Setting up RX stream \n")
-		self.rx_stream = self.sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CS16, [0, 1],  dict(WIRE=SOAPY_SDR_CS16))
+		self.rx_stream = self.sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF32, [0, 1])
 
 
 	def activate_stream_rx(self, flags = 0):
@@ -319,9 +251,8 @@ class Iris_py:
 		if r1<0: print("Problem activating stream\n")
 	
 
-	def config_beacon(self, prefix_len = 0):
+	def config_beacon(self, beacon, prefix_len = 0):
 		'''Zero-pad beacon to aling with the frame length'''
-		global beacon
 		len_beacon = len(beacon)
 		n_bz = self.n_samp - prefix_len - len_beacon
 		pad1 = np.zeros( (prefix_len), np.complex64)
@@ -344,7 +275,8 @@ class Iris_py:
     	
 		if data_i is not None: data = np.asarray(data_r, dtype=np.complex64) + 1.j*np.asarray(data_i, dtype=np.complex64)  #hack for matlab...
 
-		buf_a = cfloat2uint32(data)
+		buf_a = cfloat2uint32(data, order='QI')
+
 		#buf_b = buf_a - buf_a
 
 		self.sdr.writeRegisters("TX_RAM_A", replay_addr, buf_a.tolist() )
@@ -353,8 +285,8 @@ class Iris_py:
 	def recv_stream_tdd(self):
 		'''Read an incoming stream.'''
 		in_len  =  int(self.n_samp)
-		wave_rx_a = np.zeros((in_len), dtype=np.uint32)
-		wave_rx_b = np.zeros((in_len), dtype=np.uint32)
+		wave_rx_a = np.zeros((in_len), dtype=np.complex64)
+		wave_rx_b = np.zeros((in_len), dtype=np.complex64)
 
 		n_R = self.tdd_sched.count("R")         #How many Read frames in the tdd schedule
 		print("n_samp is: %d  \n"%self.n_samp)
@@ -364,7 +296,7 @@ class Iris_py:
 
 		print("SDR {} ".format(SoapySDR.timeNsToTicks(self.sdr.getHardwareTime(""), self.sample_rate)))
 		#print("recv_stream_tdd: wave_rx_a: \n")
-		return( uint32tocfloat(wave_rx_a) )
+		return( wave_rx_a )
 
 	def close(self):
 		'''Cleanup streams. Rest SDRs'''
@@ -383,7 +315,7 @@ class Iris_py:
 			self.sdr.writeRegister("IRIS30", RF_RST_REG, (1<<29)| 0x1)
 			self.sdr.writeRegister("IRIS30", RF_RST_REG, (1<<29))
 			self.sdr.writeRegister("IRIS30", RF_RST_REG, 0)
-			for i in range(4):
+			for i in range(8):
 				self.sdr.writeRegister("RFCORE", SCH_ADDR_REG, i) # subframe 0
 				self.sdr.writeRegister("RFCORE", SCH_MODE_REG, 0) # 01 replay
 				self.sdr.writeRegister("RFCORE", TDD_CONF_REG, 0)
@@ -455,6 +387,12 @@ if __name__ == '__main__':
 
 	#beacon1 = siso_bs.config_beacon(nsamps_pad)
 	#if mimo: beacon2 = wbz #beacon1 if both_channels else wbz
+	preambles_bs = generate_training_seq(preamble_type='gold_ifft', seq_length=128, cp=0, upsample=1)
+	beacon = preambles_bs[0, :]*.5
+	beacon1 = siso_bs.config_beacon(beacon, nsamps_pad)
+	beacon1_r = np.real(beacon1)
+	beacon1_i = np.imag(beacon1)
+	
 	
 	print("len_beacon: ")
 	print(len(beacon))
@@ -469,10 +407,10 @@ if __name__ == '__main__':
 	siso_bs.setup_stream_rx()
 
 	siso_bs.config_sdr_tdd(tdd_sched = "PGGGGGRG")
-	siso_ue.config_sdr_tdd(tdd_sched = "GGGGGGPG", is_bs = False, trigger_out = False)
+	siso_ue.config_sdr_tdd(tdd_sched = "GGGGGGPG", is_bs = False)
 
-	#siso_bs.burn_data(beacon1_r, beacon1_i)
-	siso_bs.burn_beacon()
+	siso_bs.burn_data(beacon1_r, beacon1_i)
+	#siso_bs.burn_beacon()
 	siso_ue.burn_data(wb_pilot1_r, wb_pilot1_i)
 
 	siso_bs.activate_stream_rx()
