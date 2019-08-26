9caa5a3ef610e6ef19ba39ea5d64de8ec3699b0d
Nico Barat <nicobarati@rice.edu>
2019-08-26 18:06:21 -0500

    scripts for non-OFDM modulation/demodulation
    Former-commit-id: af8810190bbc3fa571a6fffe905bebbdfa1c22f5

---
+++ MATLAB/bits2syms.m
@@ -0,0 +1,53 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%	Author(s): C. Nicolas Barati nicobarati@rice.edu 
+%
+%---------------------------------------------------------------------
+% Original code copyright Mango Communications, Inc.
+% Distributed under the WARP License http://warpproject.org/license
+% Copyright (c) 2018-2019, Rice University
+% RENEW OPEN SOURCE LICENSE: http://renew-wireless.org/license
+% ---------------------------------------------------------------------
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+function [syms, data] = bits2syms(bitstream, MOD_ORDER)
+% Theoretical BER with no chan. est error.
+
+nbits = length(bitstream);
+sym_bits = log2(MOD_ORDER);
+if mod(nbits, sym_bits) ~= 0
+    error("Length of bit stream has to be divisible by log2(MOD_ORDER)");
+end
+
+% bits to integer numbers
+bit_mat = reshape(bitstream, sym_bits, [])';
+data = bi2de(bit_mat, 'left-msb');
+
+% Functions for data -> complex symbol mapping (like qammod, avoids comm toolbox requirement)
+% These anonymous functions implement the modulation mapping from IEEE 802.11-2012 Section 18.3.5.8
+modvec_bpsk   =  (1/sqrt(2))  .* [-1 1];
+modvec_16qam  =  (1/sqrt(10)) .* [-3 -1 +3 +1];
+modvec_64qam  =  (1/sqrt(42)) .* [-7 -5 -1 -3 +7 +5 +1 +3];
+
+mod_fcn_bpsk  = @(x) complex(modvec_bpsk(1+x),0);
+mod_fcn_qpsk  = @(x) complex(modvec_bpsk(1+bitshift(x, -1)), modvec_bpsk(1+mod(x, 2)));
+mod_fcn_16qam = @(x) complex(modvec_16qam(1+bitshift(x, -2)), modvec_16qam(1+mod(x,4)));
+mod_fcn_64qam = @(x) complex(modvec_64qam(1+bitshift(x, -3)), modvec_64qam(1+mod(x,8)));
+
+% Map the data values on to complex symbols
+switch MOD_ORDER
+    case 2         % BPSK
+        syms = arrayfun(mod_fcn_bpsk, data);
+    case 4         % QPSK
+        syms = arrayfun(mod_fcn_qpsk, data);
+    case 16        % 16-QAM
+        syms = arrayfun(mod_fcn_16qam, data);
+    case 64        % 64-QAM
+        syms = arrayfun(mod_fcn_64qam, data);
+    otherwise
+        fprintf('Invalid MOD_ORDER (%d)!  Must be in [2, 4, 16, 64]\n', MOD_ORDER);
+        return;
+end
+
+end
---
+++ MATLAB/mod_demod.m
@@ -0,0 +1,79 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%	Author(s): C. Nicolas Barati nicobarati@rice.edu 
+%
+%---------------------------------------------------------------------
+% Original code copyright Mango Communications, Inc.
+% Distributed under the WARP License http://warpproject.org/license
+% Copyright (c) 2018-2019, Rice University
+% RENEW OPEN SOURCE LICENSE: http://renew-wireless.org/license
+% ---------------------------------------------------------------------
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+close all;
+clear;
+
+MOD_ORDER = 16; 
+N = 1000;
+nbits = N * log2(MOD_ORDER);
+snr = 15;
+n_ant =  1;
+awgn = 1;
+
+% Do yourselves: Create a random column vector my_bits with ones and zeros;
+mybits = randi(2, nbits,1) - 1;
+
+[syms, tx_data]  = bits2syms(mybits, MOD_ORDER);
+[rx_syms, rx_data ,bits] = syms2bits(syms,snr, n_ant,MOD_ORDER, awgn);
+
+% Plot Tx constellation
+cf = 0;
+cf = cf + 1;
+figure(cf); clf;
+ln_clr = [0.25, 0.25, 0.25];
+line([-1.5, 1.5], [0 0], 'LineStyle', '-', 'Color', ln_clr, 'LineWidth', 1);
+grid on;
+hold on;
+line([0 0], [-1.5, 1.5], 'LineStyle', '-', 'Color', ln_clr, 'LineWidth', 1);
+if MOD_ORDER ~= 2
+    plot(syms(:),'*','MarkerSize',16, 'LineWidth',2);
+else
+    plot(syms(:), 0,'*','MarkerSize',16, 'LineWidth',2);
+end
+axis square; axis(1.5*[-1 1 -1 1]);
+xlabel('Inphase');
+ylabel('Quadrature');
+title('Constellation');
+hold off;
+
+%Plot Rx constellation
+cf = cf + 1;
+figure(cf); clf;
+ln_clr = [0.25, 0.25, 0.25];
+tx_clr = [0, 0.4470, 0.7410];
+rx_clr = [0.8500, 0.3250, 0.0980];
+line([-1.5, 1.5], [0 0], 'LineStyle', '-', 'Color', ln_clr, 'LineWidth', 1);
+grid on;
+hold on;
+line([0 0], [-1.5, 1.5], 'LineStyle', '-', 'Color', ln_clr, 'LineWidth', 1);
+if MOD_ORDER ~= 2
+    plot(rx_syms(:),'o','MarkerSize',3,  'Color', rx_clr);
+    plot(syms(:),'*','MarkerSize',16, 'LineWidth',2, 'Color', tx_clr);
+    
+else
+    plot(rx_syms(:), 0,'o','MarkerSize',3, 'Color', rx_clr);
+    plot(syms(:), 0,'*','MarkerSize',16, 'LineWidth',2,  'Color', tx_clr);
+    
+end
+axis square; axis(1.5*[-1 1 -1 1]);
+xlabel('Inphase');
+ylabel('Quadrature');
+legend('Tx','Rx');
+title('Constellation');
+hold off;
+
+% Do your selves: Calculate error rates:
+sym_err = mean(tx_data ~= rx_data);
+bit_err = mean(mybits ~= bits);
+fprintf("Symbol Error Rate = %12.4e\t Bit Error Rate = %12.4e\n", sym_err, bit_err );
