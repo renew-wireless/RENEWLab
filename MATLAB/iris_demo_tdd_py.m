124ed4c809aba336f6d85c95b594b220ea62830e
Nico Barat <nicobarati@rice.edu>
2019-08-27 15:37:00 -0500

    moved mod/demod to different functions
    Former-commit-id: d530f159cdec02dd305f960abb1308419424f714

--- MATLAB/iris_demo_tdd_py.m
+++ MATLAB/iris_demo_tdd_py.m
@@ -91,31 +91,7 @@ preamble = [lts_t(33:64) lts_t lts_t];
 %% Generate a payload of random integers
 tx_data = randi(MOD_ORDER, 1, N_DATA_SYMS) - 1;
 
-% Functions for data -> complex symbol mapping (like qammod, avoids comm toolbox requirement)
-% These anonymous functions implement the modulation mapping from IEEE 802.11-2012 Section 18.3.5.8
-modvec_bpsk   =  (1/sqrt(2))  .* [-1 1];
-modvec_16qam  =  (1/sqrt(10)) .* [-3 -1 +3 +1];
-modvec_64qam  =  (1/sqrt(42)) .* [-7 -5 -1 -3 +7 +5 +1 +3];
-
-mod_fcn_bpsk  = @(x) complex(modvec_bpsk(1+x),0);
-mod_fcn_qpsk  = @(x) complex(modvec_bpsk(1+bitshift(x, -1)), modvec_bpsk(1+mod(x, 2)));
-mod_fcn_16qam = @(x) complex(modvec_16qam(1+bitshift(x, -2)), modvec_16qam(1+mod(x,4)));
-mod_fcn_64qam = @(x) complex(modvec_64qam(1+bitshift(x, -3)), modvec_64qam(1+mod(x,8)));
-
-% Map the data values on to complex symbols
-switch MOD_ORDER
-    case 2         % BPSK
-        tx_syms = arrayfun(mod_fcn_bpsk, tx_data);
-    case 4         % QPSK
-        tx_syms = arrayfun(mod_fcn_qpsk, tx_data);
-    case 16        % 16-QAM
-        tx_syms = arrayfun(mod_fcn_16qam, tx_data);
-    case 64        % 64-QAM
-        tx_syms = arrayfun(mod_fcn_64qam, tx_data);
-    otherwise
-        fprintf('Invalid MOD_ORDER (%d)!  Must be in [2, 4, 16, 64]\n', MOD_ORDER);
-        return;
-end
+tx_syms = mod_sym(tx_data, MOD_ORDER);
 
 % Reshape the symbol vector to a matrix with one column per OFDM symbol
 tx_syms_mat = reshape(tx_syms, length(SC_IND_DATA), N_OFDM_SYM);
@@ -288,24 +264,9 @@ payload_syms_mat = syms_eq_pc_mat(SC_IND_DATA, :);
 %% Demodulate
 rx_syms = reshape(payload_syms_mat, 1, N_DATA_SYMS);
 
-demod_fcn_bpsk = @(x) double(real(x)>0);
-demod_fcn_qpsk = @(x) double(2*(real(x)>0) + 1*(imag(x)>0));
-demod_fcn_16qam = @(x) (8*(real(x)>0)) + (4*(abs(real(x))<0.6325)) + (2*(imag(x)>0)) + (1*(abs(imag(x))<0.6325));
-demod_fcn_64qam = @(x) (32*(real(x)>0)) + (16*(abs(real(x))<0.6172)) + (8*((abs(real(x))<(0.9258))&&((abs(real(x))>(0.3086))))) + (4*(imag(x)>0)) + (2*(abs(imag(x))<0.6172)) + (1*((abs(imag(x))<(0.9258))&&((abs(imag(x))>(0.3086)))));
-
-switch(MOD_ORDER)
-    case 2         % BPSK
-        rx_data = arrayfun(demod_fcn_bpsk, rx_syms);
-    case 4         % QPSK
-        rx_data = arrayfun(demod_fcn_qpsk, rx_syms);
-    case 16        % 16-QAM
-        rx_data = arrayfun(demod_fcn_16qam, rx_syms);
-    case 64        % 64-QAM
-        rx_data = arrayfun(demod_fcn_64qam, rx_syms);
-end
+rx_data = demod_sym(rx_syms ,MOD_ORDER);
 
 bit_errs = length(find(dec2bin(bitxor(tx_data, rx_data),8) == '1'));
-
 
 ber_SIM(it, isnr) = bit_errs/(N_DATA_SYMS * log2(MOD_ORDER));
     
--- MATLAB/iris_demo_tdd_py_mimo.m
+++ MATLAB/iris_demo_tdd_py_mimo.m
@@ -92,31 +92,7 @@ cor_rng  = length(preamble) +  N_ZPAD_PRE + 100;
 %% Generate a payload of random integers
 tx_data = randi(MOD_ORDER, N_DATA_SYMS, N_UE) - 1;
 
-% Functions for data -> complex symbol mapping (like qammod, avoids comm toolbox requirement)
-% These anonymous functions implement the modulation mapping from IEEE 802.11-2012 Section 18.3.5.8
-modvec_bpsk   =  (1/sqrt(2))  .* [-1 1];
-modvec_16qam  =  (1/sqrt(10)) .* [-3 -1 +3 +1];
-modvec_64qam  =  (1/sqrt(42)) .* [-7 -5 -1 -3 +7 +5 +1 +3];
-
-mod_fcn_bpsk  = @(x) complex(modvec_bpsk(1+x),0);
-mod_fcn_qpsk  = @(x) complex(modvec_bpsk(1+bitshift(x, -1)), modvec_bpsk(1+mod(x, 2)));
-mod_fcn_16qam = @(x) complex(modvec_16qam(1+bitshift(x, -2)), modvec_16qam(1+mod(x,4)));    
-mod_fcn_64qam = @(x) complex(modvec_64qam(1+bitshift(x, -3)), modvec_64qam(1+mod(x,8)));
-
-% Map the data values on to complex symbols
-switch MOD_ORDER
-    case 2         % BPSK
-        tx_syms = arrayfun(mod_fcn_bpsk, tx_data);
-    case 4         % QPSK
-        tx_syms = arrayfun(mod_fcn_qpsk, tx_data);
-    case 16        % 16-QAM
-        tx_syms = arrayfun(mod_fcn_16qam, tx_data);
-    case 64        % 64-QAM
-        tx_syms = arrayfun(mod_fcn_64qam, tx_data);
-    otherwise
-        fprintf('Invalid MOD_ORDER (%d)!  Must be in [2, 4, 16, 64]\n', MOD_ORDER);
-        return;
-end
+tx_syms = mod_sym(tx_data, MOD_ORDER);
 
 % Reshape the symbol vector to a matrix with one column per OFDM symbol
 tx_syms_mat = reshape(tx_syms, length(SC_IND_DATA), N_OFDM_SYM, N_UE);
@@ -364,27 +340,7 @@ syms_eq_pc = syms_eq_pc.';
 
 
 %% Demodulate
-
-demod_fcn_bpsk = @(x) double(real(x)>0);
-demod_fcn_qpsk = @(x) double(2*(real(x)>0) + 1*(imag(x)>0));
-demod_fcn_16qam = @(x) (8*(real(x)>0)) + (4*(abs(real(x))<0.6325)) + (2*(imag(x)>0)) + (1*(abs(imag(x))<0.6325));
-demod_fcn_64qam = @(x) (32*(real(x)>0)) + (16*(abs(real(x))<0.6172)) + (8*((abs(real(x))<(0.9258))&&((abs(real(x))>(0.3086))))) + (4*(imag(x)>0)) + (2*(abs(imag(x))<0.6172)) + (1*((abs(imag(x))<(0.9258))&&((abs(imag(x))>(0.3086)))));
-
-rx_data = double.empty();
-switch(MOD_ORDER)
-    case 2         % BPSK
-        rx_data = arrayfun(demod_fcn_bpsk, syms_eq_pc );
-
-    case 4         % QPSK
-        rx_data = arrayfun(demod_fcn_qpsk, syms_eq_pc);
-        
-        
-    case 16        % 16-QAM
-        rx_data = arrayfun(demod_fcn_16qam, syms_eq_pc );  
-
-    case 64        % 64-QAM
-        rx_data = arrayfun(demod_fcn_64qam, syms_eq_pc );
-end
+rx_data = demod_sym(syms_eq_pc ,MOD_ORDER);
 
 %% Plot results
 
@@ -408,6 +364,7 @@ for sp=1:N_UE
     grid on;
     title(sprintf('UE %d Tx Waveform (Q)',sp));
 end
+
 % Rx signal
 cf = cf + 1;
 figure(cf); clf;
@@ -476,6 +433,8 @@ end
 
 %% Channel Estimates
 cf = cf + 1;
+cond_clr = [0.8500, 0.250, 0.1980];
+
 bw_span = (20/N_SC) * (-(N_SC/2):(N_SC/2 - 1)).';
 
 figure(cf); clf;
@@ -493,7 +452,8 @@ for ibs = 1:N_BS_NODE
 end
 
 subplot(N_BS_NODE+1,1,N_BS_NODE+1);
-bar(bw_span, fftshift(channel_condition_db) ,1,'LineWidth', 1);
+bh = bar(bw_span, fftshift(channel_condition_db) ,1, 'LineWidth', 1);
+set(bh,'FaceColor',cond_clr);
 axis([min(bw_span) max(bw_span) 0 max(channel_condition_db)+1])
 grid on;
 title('Channel Condition (dB)')
@@ -560,4 +520,6 @@ fprintf('Num Bytes:   %d\n', N_UE*N_DATA_SYMS * log2(MOD_ORDER) / 8);
 fprintf('Sym Errors:  %d (of %d total symbols)\n', sym_errs, N_UE * N_DATA_SYMS);
 fprintf('Bit Errors:  %d (of %d total bits)\n', bit_errs, N_UE*N_DATA_SYMS * log2(MOD_ORDER));
 
-snr_mat
+fprintf("SNRs (dB): \n");
+fprintf("%.2f\t", snr_mat);
+fprintf("\n");
