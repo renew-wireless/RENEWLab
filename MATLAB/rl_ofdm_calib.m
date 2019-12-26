c2532a9e99678c2cdfcdf0483d34d8a0f392fcaa
rdoost <doost@rice.edu>
2019-12-26 13:41:43 -0600

    changed frequency to 2.5G and reverted the gain
    Former-commit-id: b2438f55d0b4e579435d7b3788e6b0c681fbf264

--- MATLAB/rl_ofdm_calib.m
+++ MATLAB/rl_ofdm_calib.m
@@ -24,10 +24,10 @@ WRITE_PNG_FILES         = 0;           % Enable writing plots to PNG
 
 %Iris params:
 USE_HUB                 = 0;
-TX_FRQ                  = 3.6e9;    
+TX_FRQ                  = 2.5e9;    
 RX_FRQ                  = TX_FRQ;
-TX_GN                   = 70;
-RX_GN                   = 50;
+TX_GN                   = 40;
+RX_GN                   = 20;
 SMPL_RT                 = 5e6;  
 N_FRM                   = 1;
 bs_ids = string.empty();
@@ -71,7 +71,7 @@ else
     hub_id = [];
 end
 
-bs_ids = ["RF3E000208", "RF3E000636", "RF3E000632", "RF3E000568", "RF3E000558", "RF3E000633", "RF3E000566", "RF3E000635"];
+bs_ids = ["RF3E000183", "RF3E000152", "RF3E000123", "RF3E000178", "RF3E000113", "RF3E000176", "RF3E000132", "RF3E000108"];
 
 bs_sched = ["PGRG", "RGPG"];  % All BS schedule, Ref Schedule
 N_BS_NODE = length(bs_ids);
