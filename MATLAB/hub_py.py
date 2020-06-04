b0a392d25b370bf4d4ebb162f0a1af3d32334a96
rdoost <doost@rice.edu>
2020-06-03 23:00:56 -0500

    remove trigger argument from set_trigger
    Former-commit-id: 21ee2560426cd8cf76ab08d5cd93c125ef837a2e

--- MATLAB/hub_py.py
+++ MATLAB/hub_py.py
@@ -1,48 +1,49 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 """ 
-	Author(s): C. Nicolas Barati nicobarati@rice.edu 
-		Rahman Doost-Mohamamdy: doost@rice.edu
-		Oscar Bejarano: obejarano@rice.edu
+        Author(s): C. Nicolas Barati nicobarati@rice.edu 
+                Rahman Doost-Mohamamdy: doost@rice.edu
+                Oscar Bejarano: obejarano@rice.edu
 
 ---------------------------------------------------------------------
-	Copyright (c) 2018-2019, Rice University
-	RENEW OPEN SOURCE LICENSE: http://renew-wireless.org/license
+        Copyright (c) 2018-2019, Rice University
+        RENEW OPEN SOURCE LICENSE: http://renew-wireless.org/license
 ---------------------------------------------------------------------
 """
 
 import sys
 import SoapySDR
-from SoapySDR import * #SOAPY_SDR_ constants
+from SoapySDR import *  # SOAPY_SDR_ constants
 import numpy as np
 import time
 import os
 
-#######################################				
-#######	Hub Class:		#######
+#######################################
+####### Hub Class:              #######
 #######################################
 
+
 class Hub_py:
-	'''
-		Iris python class. To act as a middle layer between Matlab classes and SoapySDRs
-	'''
-	def __init__(self,
-		serial_id = None,
-	):
+        '''
+                Iris python class. To act as a middle layer between Matlab classes and SoapySDRs
+        '''
 
-		if serial_id is not None: 
-			self.sdr = SoapySDR.Device(dict(driver="remote", serial = serial_id))
-			self.serial_id = serial_id
-		else:
-			self.sdr = None
+        def __init__(self,
+                     serial_id=None,
+                     ):
 
+                if serial_id is not None:
+                        self.sdr = SoapySDR.Device(
+                            dict(driver="remote", serial=serial_id))
+                        if self.sdr == None:
+                            print("Error in initializing the hub!")
+                else:
+                        self.sdr = None
 
     # Set trigger:
-	def set_trigger(self, trig = False):
-		if bool(trig) is True:
-			self.sdr.writeSetting("TRIGGER_GEN", "")
-        
-	def sync_delays(self):
-		'''Synchronise delays.'''
-		self.sdr.writeSetting("SYNC_DELAYS", "")		
+        def set_trigger(self):
+                self.sdr.writeSetting("TRIGGER_GEN", "")
 
+        def sync_delays(self):
+                '''Synchronise delays.'''
+                self.sdr.writeSetting("SYNC_DELAYS", "")
