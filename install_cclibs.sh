18dcfdad839a6edb2e0d6f11f4046d810f40a69d
rdoost <doost@rice.edu>
2020-07-01 10:44:57 -0500

    temp fix for json-related compile issue due to recent changes in nlohman-json package
    Former-commit-id: 48d2243a2a0edbcba0c762a4171e5d1974a559bb

--- install_cclibs.sh
+++ install_cclibs.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 HDF5_LIB=libhdf5-cpp-100
-ITPP_LIB=libitpp-dev
 if [[ `lsb_release -rs` == "16.04" ]]
 then
     HDF5_LIB=libhdf5-cpp-11
 fi
-sudo apt -y install libhdf5-dev $HDF5_LIB $ITPP_LIB
+sudo apt -y install libhdf5-dev $HDF5_LIB
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LIBDIR="$BASEDIR"/CC/Sounder/lib 
@@ -16,6 +15,7 @@ mkdir -p deps
 cd deps/
 git clone https://github.com/nlohmann/json.git
 cd json/
+git checkout eab68e77
 mkdir -p build/
 cd build/
 cmake ..
