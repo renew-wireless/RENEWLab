04508e983016aab2ffd8a95f2d86a3d5d6610106
Min Zhang <min.zhang@rice.edu>
2020-10-20 11:41:32 -0500

    Make BS and client Irises checking configurable in WEBGUI Change git version notice in config_ci.sh Remove RENEW logo in WEBGUI README.md
    See #22, #24

--- WEBGUI/iris_get_status.py
+++ WEBGUI/iris_get_status.py
@@ -21,6 +21,7 @@ LMS7 = "LMS7 Temperature"
 RX = "Rx temperature"
 ZYNQ = "Zynq Temperature"
 TX = "Tx temperature"
+BS_IRIS_ONLY = os.environ.get('CHECK_BS_IRIS_ONLY') or '1'  # 0: BS & client
 
 
 def get_iris_sns_on_network():
@@ -30,7 +31,12 @@ def get_iris_sns_on_network():
     :return irises: A list of Iris serial numbers detected on network.
     """
     
-    irises = os.popen("SoapySDRUtil --find --sparse").read()
+    for i in range(3):
+        print("Try {}".format(i))
+        irises = os.popen("SoapySDRUtil --find --sparse").read()
+        # The above command sometimes returns empty. 
+        if len(irises) != 0:
+            break
     irises = irises.splitlines()
     # Filter out none Iris devices
     irises = list(filter(lambda iris: (iris.find("IRIS") != -1), irises))
@@ -60,33 +66,58 @@ def get_sensors():
         try:
             SoapySDR.Device.enumerate({'remote:timeout': '1000000'})
             sdr = SoapySDR.Device(dict(driver="iris", serial=iris))
-            sensors = sdr.listSensors()
-            for sensor in sensors:
-                info = sdr.getSensorInfo(sensor)
-                try:
-                    stat[info.name] = sdr.readSensor(sensor)
-                except Exception:
-                    stat[info.name] = '-100.0'
-                    pass
-                    
-            status['sn'] = iris
-            status['LMS7temp'] = stat[LMS7] if LMS7 in stat else '-101.0'
-            status['ZYNQtemp'] = stat[ZYNQ] if ZYNQ in stat else '101.0'
-            status['TXtemp'] = stat[TX] if TX in stat else '-101.0'
-            status['RXtemp'] = stat[RX] if RX in stat else '-101.0'
-            status['LMS7temp'] = "{:.2f}".format(float(status['LMS7temp']))
-            status['ZYNQtemp'] = "{:.2f}".format(float(status['ZYNQtemp']))
-            status['TXtemp'] = "{:.2f}".format(float(status['TXtemp']))
-            status['RXtemp'] = "{:.2f}".format(float(status['RXtemp']))
+            # Check only base station Irises. 
+            if BS_IRIS_ONLY == '1':
+                if "ue" not in sdr.getHardwareInfo()["fpga"]:
+                    sensors = sdr.listSensors()
+                    for sensor in sensors:
+                        info = sdr.getSensorInfo(sensor)
+                        try:
+                            stat[info.name] = sdr.readSensor(sensor)
+                        except Exception:
+                            stat[info.name] = '-100.0'
+                            pass
+                        
+                    status['sn'] = iris
+                    status['LMS7temp'] = stat[LMS7] if LMS7 in stat else '-101.0'
+                    status['ZYNQtemp'] = stat[ZYNQ] if ZYNQ in stat else '101.0'
+                    status['TXtemp'] = stat[TX] if TX in stat else '-101.0'
+                    status['RXtemp'] = stat[RX] if RX in stat else '-101.0'
+                    status['LMS7temp'] = "{:.2f}".format(float(status['LMS7temp']))
+                    status['ZYNQtemp'] = "{:.2f}".format(float(status['ZYNQtemp']))
+                    status['TXtemp'] = "{:.2f}".format(float(status['TXtemp']))
+                    status['RXtemp'] = "{:.2f}".format(float(status['RXtemp']))
+                    irises_sensors.append(status)
+            else:
+                sensors = sdr.listSensors()
+                for sensor in sensors:
+                    info = sdr.getSensorInfo(sensor)
+                    try:
+                        stat[info.name] = sdr.readSensor(sensor)
+                    except Exception:
+                        stat[info.name] = '-100.0'
+                        pass
+                        
+                status['sn'] = iris
+                status['LMS7temp'] = stat[LMS7] if LMS7 in stat else '-101.0'
+                status['ZYNQtemp'] = stat[ZYNQ] if ZYNQ in stat else '101.0'
+                status['TXtemp'] = stat[TX] if TX in stat else '-101.0'
+                status['RXtemp'] = stat[RX] if RX in stat else '-101.0'
+                status['LMS7temp'] = "{:.2f}".format(float(status['LMS7temp']))
+                status['ZYNQtemp'] = "{:.2f}".format(float(status['ZYNQtemp']))
+                status['TXtemp'] = "{:.2f}".format(float(status['TXtemp']))
+                status['RXtemp'] = "{:.2f}".format(float(status['RXtemp']))
+                irises_sensors.append(status)
         except:
             status['sn'] = iris
             status['LMS7temp'] = '-102.0'
             status['ZYNQtemp'] = '-102.0'
             status['TXtemp'] = '-102.0'
             status['RXtemp'] = '-102.0'
+            irises_sensors.append(status)
             pass
 
-        irises_sensors.append(status)
+        # irises_sensors.append(status)
 
     return irises_sensors
 
@@ -138,8 +169,10 @@ def check_errors(irises_sensors, thresh):
 
 
 if __name__ == "__main__":
+    # get_iris_sns_on_network()
     irises_sensors = get_sensors()
     print(irises_sensors)
+    print(len(irises_sensors))
     print("Overheating>50C {}".format(check_overheat(irises_sensors, "50.0")))
     print("Overheating>70C {}".format(check_overheat(irises_sensors, "70.0")))
     print("Error?<=0C {}".format(check_errors(irises_sensors, "0.0")))
