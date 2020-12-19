d1c6161513f9262126e3122d6e3bdd99d05f3351
rdoosty <doost@rice.edu>
2020-12-18 22:25:16 -0600

    fix getSequence function call in testbench

--- CC/Sounder/tests/comms-func/test-main.cc
+++ CC/Sounder/tests/comms-func/test-main.cc
@@ -43,7 +43,9 @@ int main(int argc, char const* argv[])
     testDataCF32.push_back(std::complex<float>(.9, .0));
     std::vector<std::complex<int16_t>> testDataCS16;
     for (size_t i = 0; i < testDataCF32.size(); i++)
-        testDataCS16.push_back(std::complex<int16_t>((int16_t)(testDataCF32[i].real() * 32768), (int16_t)(testDataCF32[i].imag() * 32768)));
+        testDataCS16.push_back(
+            std::complex<int16_t>((int16_t)(testDataCF32[i].real() * 32768),
+                (int16_t)(testDataCF32[i].imag() * 32768)));
 
     auto testAbs0 = CommsLib::abs2_avx(testDataCF32);
     auto testAbs1 = CommsLib::abs2_avx(testDataCS16);
@@ -75,12 +77,16 @@ int main(int argc, char const* argv[])
     auto testCS2 = CommsLib::complex_mult_avx(testDataCS16, testDataCS16, true);
     auto testCF2 = CommsLib::complex_mult_avx(testDataCF32, testDataCF32, true);
     for (size_t i = 0; i < testCS2.size(); i++)
-        std::cout << "Conj True - CS Result: (" << testCS2[i].real() / 32768.0 << "," << testCS2[i].imag() / 32768.0 << "), CF Result: " << testCF2[i] << std::endl;
+        std::cout << "Conj True - CS Result: (" << testCS2[i].real() / 32768.0
+                  << "," << testCS2[i].imag() / 32768.0
+                  << "), CF Result: " << testCF2[i] << std::endl;
 
     testCS2 = CommsLib::complex_mult_avx(testDataCS16, testDataCS16, false);
     testCF2 = CommsLib::complex_mult_avx(testDataCF32, testDataCF32, false);
     for (size_t i = 0; i < testCS2.size(); i++)
-        std::cout << "Conj False - CS Result: (" << testCS2[i].real() / 32768.0 << "," << testCS2[i].imag() / 32768.0 << "), CF Result: " << testCF2[i] << std::endl;
+        std::cout << "Conj False - CS Result: (" << testCS2[i].real() / 32768.0
+                  << "," << testCS2[i].imag() / 32768.0
+                  << "), CF Result: " << testCF2[i] << std::endl;
 
     /*
      * test mult_avx
@@ -89,51 +95,95 @@ int main(int argc, char const* argv[])
     bool conj = true;
     auto testMultRes = CommsLib::auto_corr_mult_avx(testDataCF32, dly, conj);
     auto testMultResCS = CommsLib::auto_corr_mult_avx(testDataCS16, dly, conj);
-    std::cout << "\nTesting auto_corr_mult_avx (dly = " << dly << ", conj = " << (conj ? "true" : "false")
-              << "):\n";
+    std::cout << "\nTesting auto_corr_mult_avx (dly = " << dly
+              << ", conj = " << (conj ? "true" : "false") << "):\n";
     for (size_t i = 0; i < testMultRes.size(); i++) {
         if ((int)i < dly)
-            std::cout << "Float Op Result: " << testMultRes[i] << ", INT Op Result: (" << testMultResCS[i].real() / 32768.0 << "," << testMultResCS[i].imag() / 32668.0 << "), GT: (0, 0)" << std::endl;
+            std::cout << "Float Op Result: " << testMultRes[i]
+                      << ", INT Op Result: ("
+                      << testMultResCS[i].real() / 32768.0 << ","
+                      << testMultResCS[i].imag() / 32668.0 << "), GT: (0, 0)"
+                      << std::endl;
         else
-            std::cout << "Float Op Result: " << testMultRes[i] << ", INT Op Result: (" << testMultResCS[i].real() / 32768.0 << "," << testMultResCS[i].imag() / 32668.0 << "), GT:" << testDataCF32[i] * (conj ? std::conj(testDataCF32[i - dly]) : testDataCF32[i - dly]) << std::endl;
+            std::cout << "Float Op Result: " << testMultRes[i]
+                      << ", INT Op Result: ("
+                      << testMultResCS[i].real() / 32768.0 << ","
+                      << testMultResCS[i].imag() / 32668.0 << "), GT:"
+                      << testDataCF32[i]
+                    * (conj ? std::conj(testDataCF32[i - dly])
+                            : testDataCF32[i - dly])
+                      << std::endl;
     }
 
     conj = false;
     testMultRes = CommsLib::auto_corr_mult_avx(testDataCF32, dly, conj);
     testMultResCS = CommsLib::auto_corr_mult_avx(testDataCS16, dly, conj);
-    std::cout << "\nTesting auto_corr_mult_avx (dly = " << dly << ", conj = " << (conj ? "true" : "false")
-              << "):\n";
+    std::cout << "\nTesting auto_corr_mult_avx (dly = " << dly
+              << ", conj = " << (conj ? "true" : "false") << "):\n";
     for (size_t i = 0; i < testMultRes.size(); i++) {
         if ((int)i < dly)
-            std::cout << "Float Op Result: " << testMultRes[i] << ", INT Op Result: (" << testMultResCS[i].real() / 32768.0 << "," << testMultResCS[i].imag() / 32668.0 << "), GT: (0, 0)" << std::endl;
+            std::cout << "Float Op Result: " << testMultRes[i]
+                      << ", INT Op Result: ("
+                      << testMultResCS[i].real() / 32768.0 << ","
+                      << testMultResCS[i].imag() / 32668.0 << "), GT: (0, 0)"
+                      << std::endl;
         else
-            std::cout << "Float Op Result: " << testMultRes[i] << ", INT Op Result: (" << testMultResCS[i].real() / 32768.0 << "," << testMultResCS[i].imag() / 32668.0 << "), GT:" << testDataCF32[i] * (conj ? std::conj(testDataCF32[i - dly]) : testDataCF32[i - dly]) << std::endl;
+            std::cout << "Float Op Result: " << testMultRes[i]
+                      << ", INT Op Result: ("
+                      << testMultResCS[i].real() / 32768.0 << ","
+                      << testMultResCS[i].imag() / 32668.0 << "), GT:"
+                      << testDataCF32[i]
+                    * (conj ? std::conj(testDataCF32[i - dly])
+                            : testDataCF32[i - dly])
+                      << std::endl;
     }
 
     dly = 1;
     conj = true;
     testMultRes = CommsLib::auto_corr_mult_avx(testDataCF32, dly, conj);
     testMultResCS = CommsLib::auto_corr_mult_avx(testDataCS16, dly, conj);
-    std::cout << "\nTesting auto_corr_mult_avx (dly = " << dly << ", conj = " << (conj ? "true" : "false")
-              << "):\n";
+    std::cout << "\nTesting auto_corr_mult_avx (dly = " << dly
+              << ", conj = " << (conj ? "true" : "false") << "):\n";
     for (size_t i = 0; i < testMultRes.size(); i++) {
         if ((int)i < dly)
-            std::cout << "Float Op Result: " << testMultRes[i] << ", INT Op Result: (" << testMultResCS[i].real() / 32768.0 << "," << testMultResCS[i].imag() / 32668.0 << "), GT: (0, 0)" << std::endl;
+            std::cout << "Float Op Result: " << testMultRes[i]
+                      << ", INT Op Result: ("
+                      << testMultResCS[i].real() / 32768.0 << ","
+                      << testMultResCS[i].imag() / 32668.0 << "), GT: (0, 0)"
+                      << std::endl;
         else
-            std::cout << "Float Op Result: " << testMultRes[i] << ", INT Op Result: (" << testMultResCS[i].real() / 32768.0 << "," << testMultResCS[i].imag() / 32668.0 << "), GT:" << testDataCF32[i] * (conj ? std::conj(testDataCF32[i - dly]) : testDataCF32[i - dly]) << std::endl;
+            std::cout << "Float Op Result: " << testMultRes[i]
+                      << ", INT Op Result: ("
+                      << testMultResCS[i].real() / 32768.0 << ","
+                      << testMultResCS[i].imag() / 32668.0 << "), GT:"
+                      << testDataCF32[i]
+                    * (conj ? std::conj(testDataCF32[i - dly])
+                            : testDataCF32[i - dly])
+                      << std::endl;
     }
 
     dly = 2;
     conj = false;
     testMultRes = CommsLib::auto_corr_mult_avx(testDataCF32, dly, conj);
     testMultResCS = CommsLib::auto_corr_mult_avx(testDataCS16, dly, conj);
-    std::cout << "\nTesting auto_corr_mult_avx (dly = " << dly << ", conj = " << (conj ? "true" : "false")
-              << "):\n";
+    std::cout << "\nTesting auto_corr_mult_avx (dly = " << dly
+              << ", conj = " << (conj ? "true" : "false") << "):\n";
     for (size_t i = 0; i < testMultRes.size(); i++) {
         if ((int)i < dly)
-            std::cout << "Float Op Result: " << testMultRes[i] << ", INT Op Result: (" << testMultResCS[i].real() / 32768.0 << "," << testMultResCS[i].imag() / 32668.0 << "), GT: (0, 0)" << std::endl;
+            std::cout << "Float Op Result: " << testMultRes[i]
+                      << ", INT Op Result: ("
+                      << testMultResCS[i].real() / 32768.0 << ","
+                      << testMultResCS[i].imag() / 32668.0 << "), GT: (0, 0)"
+                      << std::endl;
         else
-            std::cout << "Float Op Result: " << testMultRes[i] << ", INT Op Result: (" << testMultResCS[i].real() / 32768.0 << "," << testMultResCS[i].imag() / 32668.0 << "), GT:" << testDataCF32[i] * (conj ? std::conj(testDataCF32[i - dly]) : testDataCF32[i - dly]) << std::endl;
+            std::cout << "Float Op Result: " << testMultRes[i]
+                      << ", INT Op Result: ("
+                      << testMultResCS[i].real() / 32768.0 << ","
+                      << testMultResCS[i].imag() / 32668.0 << "), GT:"
+                      << testDataCF32[i]
+                    * (conj ? std::conj(testDataCF32[i - dly])
+                            : testDataCF32[i - dly])
+                      << std::endl;
     }
 
     /*
@@ -152,10 +202,14 @@ int main(int argc, char const* argv[])
 
     std::vector<std::complex<int16_t>> testCorrSeqCS16;
     for (size_t i = 0; i < testCorrSeqCF32.size(); i++)
-        testCorrSeqCS16.push_back(std::complex<int16_t>((int16_t)(testCorrSeqCF32[i].real() * 32768), (int16_t)(testCorrSeqCF32[i].imag() * 32768)));
+        testCorrSeqCS16.push_back(
+            std::complex<int16_t>((int16_t)(testCorrSeqCF32[i].real() * 32768),
+                (int16_t)(testCorrSeqCF32[i].imag() * 32768)));
 
-    auto testCorrResCF32 = CommsLib::correlate_avx(testDataCF32, testCorrSeqCF32);
-    auto testCorrResCS16 = CommsLib::correlate_avx(testDataCS16, testCorrSeqCS16);
+    auto testCorrResCF32
+        = CommsLib::correlate_avx(testDataCF32, testCorrSeqCF32);
+    auto testCorrResCS16
+        = CommsLib::correlate_avx(testDataCS16, testCorrSeqCS16);
 
     //std::vector<std::complex<double>> testCorrSeq2;
     //std::vector<std::complex<double>> iq;
@@ -166,8 +220,10 @@ int main(int argc, char const* argv[])
     //}
     //auto testCorrRef = CommsLib::convolve(iq, testCorrSeq2);
     std::vector<std::complex<float>> testCorrRef;
-    std::vector<std::complex<float>> testDataExtend(testCorrSeqCF32.size() - 1, 0);
-    testDataExtend.insert(testDataExtend.end(), testDataCF32.begin(), testDataCF32.end());
+    std::vector<std::complex<float>> testDataExtend(
+        testCorrSeqCF32.size() - 1, 0);
+    testDataExtend.insert(
+        testDataExtend.end(), testDataCF32.begin(), testDataCF32.end());
     for (size_t i = 0; i < testDataExtend.size(); i++) {
         std::complex<float> acc(0);
         for (size_t j = 0; j < testCorrSeqCF32.size(); j++) {
@@ -180,7 +236,10 @@ int main(int argc, char const* argv[])
     }
     for (size_t i = 0; i < testCorrResCF32.size(); i++) {
         //std::cout << testCorrResCF32[i] << ", GT:" << testCorrRef[i] << std::endl;
-        std::cout << "Float Op Result: " << testCorrResCF32[i] << ", INT Op Result: (" << testCorrResCS16[i].real() / 32768.0 << "," << testCorrResCS16[i].imag() / 32768.0 << "), GT:" << testCorrRef[i] << std::endl;
+        std::cout << "Float Op Result: " << testCorrResCF32[i]
+                  << ", INT Op Result: (" << testCorrResCS16[i].real() / 32768.0
+                  << "," << testCorrResCS16[i].imag() / 32768.0
+                  << "), GT:" << testCorrRef[i] << std::endl;
     }
 
     std::cout << "\nTesting Real Correlate:\n";
@@ -189,11 +248,14 @@ int main(int argc, char const* argv[])
     std::vector<int16_t> testAbsInt;
     for (size_t i = 0; i < testAbs0.size(); i++)
         testAbsInt.push_back(std::abs((int16_t)(testAbs0[i] * 32768)));
-    auto testCorrResFloat = CommsLib::correlate_avx_s(testAbs0, testCorrSeqFloat);
-    auto testCorrResInt = CommsLib::correlate_avx_si(testAbsInt, testCorrSeqInt);
+    auto testCorrResFloat
+        = CommsLib::correlate_avx_s(testAbs0, testCorrSeqFloat);
+    auto testCorrResInt
+        = CommsLib::correlate_avx_si(testAbsInt, testCorrSeqInt);
     std::vector<float> testCorrRefReal;
     std::vector<float> testDataExtend1(testCorrSeqFloat.size() - 1, 0);
-    testDataExtend1.insert(testDataExtend1.end(), testAbs0.begin(), testAbs0.end());
+    testDataExtend1.insert(
+        testDataExtend1.end(), testAbs0.begin(), testAbs0.end());
     for (size_t i = 0; i < testDataExtend1.size(); i++) {
         float acc(0);
         for (size_t j = 0; j < testCorrSeqFloat.size(); j++) {
@@ -205,7 +267,9 @@ int main(int argc, char const* argv[])
         testCorrRefReal.push_back(acc);
     }
     for (size_t i = 0; i < testCorrResFloat.size(); i++) {
-        std::cout << "Float Op Result: " << testCorrResFloat[i] << ", INT Op Result: (" << testCorrResInt[i] / 32768.0 << "), GT:" << testCorrRefReal[i] << std::endl;
+        std::cout << "Float Op Result: " << testCorrResFloat[i]
+                  << ", INT Op Result: (" << testCorrResInt[i] / 32768.0
+                  << "), GT:" << testCorrRefReal[i] << std::endl;
     }
 #else
     /*
@@ -214,18 +278,21 @@ int main(int argc, char const* argv[])
     std::cout << "Testing find_beacon_avx:\n";
 
     // generate beacon
-    std::vector<std::vector<double>> gold_ifft = CommsLib::getSequence(128, CommsLib::GOLD_IFFT);
-    std::vector<std::complex<int16_t>> gold_ifft_ci16 = Utils::double_to_cint16(gold_ifft);
+    std::vector<std::vector<double>> gold_ifft
+        = CommsLib::getSequence(CommsLib::GOLD_IFFT);
+    std::vector<std::complex<int16_t>> gold_ifft_ci16
+        = Utils::double_to_cint16(gold_ifft);
 
     int goldReps = 2;
     std::vector<std::complex<int16_t>> beacon_ci16;
     for (int i = 0; i < goldReps; i++) {
-        beacon_ci16.insert(beacon_ci16.end(), gold_ifft_ci16.begin(), gold_ifft_ci16.end());
+        beacon_ci16.insert(
+            beacon_ci16.end(), gold_ifft_ci16.begin(), gold_ifft_ci16.end());
     }
 
     size_t beaconSize = beacon_ci16.size();
     const int seqLen = 128;
-    auto gold_seq = CommsLib::getSequence(seqLen, CommsLib::GOLD_IFFT);
+    auto gold_seq = CommsLib::getSequence(CommsLib::GOLD_IFFT);
     std::vector<std::complex<float>> gold_sym_orig(seqLen);
     for (size_t i = 0; i < seqLen; i++) {
         gold_sym_orig[i] = std::complex<float>(gold_seq[0][i], gold_seq[1][i]);
@@ -260,9 +327,14 @@ int main(int argc, char const* argv[])
     clock_gettime(CLOCK_MONOTONIC, &tv);
     int sync_index = CommsLib::find_beacon_avx(buffs, gold_sym_orig);
     clock_gettime(CLOCK_MONOTONIC, &tv2);
-    double diff = ((tv2.tv_sec - tv.tv_sec) * 1e9 + (tv2.tv_nsec - tv.tv_nsec)) / 1e3;
-    std::cout << "SYNC Found at index " << sync_index - 2 * seqLen + 1 << std::endl;
-    std::cout << "TEST " << (((int)prefix == sync_index - 2 * seqLen + 1) ? "PASSED" : "FAILED") << std::endl;
+    double diff
+        = ((tv2.tv_sec - tv.tv_sec) * 1e9 + (tv2.tv_nsec - tv.tv_nsec)) / 1e3;
+    std::cout << "SYNC Found at index " << sync_index - 2 * seqLen + 1
+              << std::endl;
+    std::cout << "TEST "
+              << (((int)prefix == sync_index - 2 * seqLen + 1) ? "PASSED"
+                                                               : "FAILED")
+              << std::endl;
     std::cout << "Correlation took " << diff << " usec" << std::endl;
 #else
     size_t symbolsPerFrame = 5;
@@ -274,15 +346,17 @@ int main(int argc, char const* argv[])
     FILE* fp = fopen(filename.c_str(), "rb");
     std::vector<int> index(samps_per_frame);
     for (size_t i = 0; i < num_frames; i++) {
-        size_t bytes = fread(buff0.data(), sizeof(float) * 2, samps_per_frame, fp);
+        size_t bytes
+            = fread(buff0.data(), sizeof(float) * 2, samps_per_frame, fp);
         if (bytes < samps_per_frame) {
             printf("Error reading batch %zu, Exitting...\n", i);
             break;
         }
         int sync_index = CommsLib::find_beacon_avx(buff0, gold_sym_orig);
-	if (sync_index >= 0 && sync_index < samps_per_frame)
-		index[sync_index]++;
-        printf("Frame: %zu, sync_idx: %zu, total_idx: %d\n", i, sync_index, (sync_index + i * samps_per_frame));
+        if (sync_index >= 0 && sync_index < samps_per_frame)
+            index[sync_index]++;
+        printf("Frame: %zu, sync_idx: %zu, total_idx: %d\n", i, sync_index,
+            (sync_index + i * samps_per_frame));
     }
     for (size_t i = 0; i < samps_per_frame; i++) {
         if (index[i] > 0)
