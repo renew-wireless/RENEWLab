41f0bd603cda67c0695068e43744a78671a021ab
rdoosty <doost@rice.edu>
2021-03-25 10:54:19 -0500

    use #if defined instead of #ifdef

--- CC/Sounder/receiver.cc
+++ CC/Sounder/receiver.cc
@@ -633,7 +633,7 @@ void Receiver::clientSyncTxRx(int tid)
             }
         }
         //TODO syncbuff0 is sloppy here since we recevied into syncrxbuff.data(), r bytes.
-#ifdef __X86_64__
+#if defined(__X86_64__)
         sync_index = CommsLib::find_beacon_avx(syncbuff0, config_->gold_cf32());
 #else
         sync_index = CommsLib::find_beacon(syncbuff0);
@@ -702,7 +702,7 @@ void Receiver::clientSyncTxRx(int tid)
                     //TODO: Remove the copy and direct access to syncbuff0
                     std::vector<std::complex<float>> radio_rx_data(
                         syncbuff0.begin(), syncbuff0.begin() + r);
-#ifdef __X86_64__
+#if defined(__X86_64__)
                     sync_index = CommsLib::find_beacon_avx(
                         radio_rx_data, config_->gold_cf32());
 #else
