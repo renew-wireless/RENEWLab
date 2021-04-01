01c4c6d05e2ad1b261e072c3d23e9a04d1c2e6ae
rdoosty <doost@rice.edu>
2021-04-01 13:52:52 -0500

    pass on arguments including JSON path using gflags. New argument for data generation

--- CC/Sounder/main.cc
+++ CC/Sounder/main.cc
@@ -12,36 +12,34 @@
 
 #include "include/recorder.h"
 #include "include/signalHandler.hpp"
+#include <gflags/gflags.h>
 
-int main(int argc, char const* argv[])
+DEFINE_uint64(frame_data_gen, false,
+    "Generate random bits for uplink transmissions, otherwise read from file!");
+DEFINE_string(conf, "files/conf.json", "JSON configuration file name");
+
+int main(int argc, char* argv[])
 {
-    int ret;
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " CONFIG_FILE_NAME" << std::endl;
-        return EXIT_FAILURE;
-    }
-    if (strcmp(argv[1], "--help") == 0 or strcmp(argv[1], "-h") == 0) {
-        std::cerr << "Usage: " << argv[0] << " CONFIG_FILE_NAME" << std::endl;
-        return EXIT_SUCCESS;
-    }
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    Config config(FLAGS_conf);
+    int ret = EXIT_SUCCESS;
+    if (!FLAGS_frame_data_gen) {
+        try {
+            SignalHandler signalHandler;
 
-    Config config(argv[1]);
-
-    try {
-        SignalHandler signalHandler;
-
-        // Register signal handler to handle kill signal
-        signalHandler.setupSignalHandlers();
-        Sounder::Recorder dr(&config);
-        dr.do_it();
-        ret = EXIT_SUCCESS;
-    } catch (SignalException& e) {
-        std::cerr << "SignalException: " << e.what() << std::endl;
-        ret = EXIT_FAILURE;
-    } catch (const std::exception& exc) {
-        std::cerr << "Program terminated Exception: " << exc.what()
-                  << std::endl;
-        ret = EXIT_FAILURE;
+            // Register signal handler to handle kill signal
+            signalHandler.setupSignalHandlers();
+            Sounder::Recorder dr(&config);
+            dr.do_it();
+            ret = EXIT_SUCCESS;
+        } catch (SignalException& e) {
+            std::cerr << "SignalException: " << e.what() << std::endl;
+            ret = EXIT_FAILURE;
+        } catch (const std::exception& exc) {
+            std::cerr << "Program terminated Exception: " << exc.what()
+                      << std::endl;
+            ret = EXIT_FAILURE;
+        }
     }
     return ret;
 }
