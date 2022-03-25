f279d7e29030f8611f0d2a78b6ffac6384316e44
Tom <82247342+Shadowcaster77@users.noreply.github.com>
2022-03-25 15:21:41 -0400

    Update ClientRadioSet.h

--- CC/Sounder/include/ClientRadioSet.h
+++ CC/Sounder/include/ClientRadioSet.h
@@ -1,75 +1,40 @@
 #include "config.h"
 #include <SoapySDR/Device.hpp>
-#include <chrono>
-#include <complex>
-#include <csignal>
-#include <cstddef>
-#include <cstdint>
-#include <cstdlib>
-#include <fstream>
-#include <iostream>
-#include <string>
 
+#ifndef CLIENT_RADIO_SET_H
+#define CLIENT_RADIO_SET_H
 
-
-#include <SoapySDR/Time.hpp>
-#include <uhd/usrp/multi_usrp.hpp>
-#include <uhd/stream.hpp>
-#include <uhd/utils/thread_priority.hpp>
-#include <uhd/utils/safe_main.hpp>
-#include <uhd/usrp/multi_usrp.hpp>
-#include <boost/program_options.hpp>
-#include <boost/format.hpp>
-#include <boost/thread.hpp>
-#include <boost/lexical_cast.hpp>
-#include <boost/algorithm/string.hpp>
-#include <iostream>
-#include <complex>
+#pragma once
 
 class Radio;
 
-class BaseRadioSet {
+class ClientRadioSet {
 public:
-    BaseRadioSet(Config* cfg);
-    ~BaseRadioSet(void);
-    void radioTx(const void* const* buffs);
-    void radioRx(void* const* buffs);
-    int radioTx(size_t radio_id, size_t cell_id, const void* const* buffs,
-        int flags, long long& frameTime);
-    int radioRx(size_t radio_id, size_t cell_id, void* const* buffs,
+    ClientRadioSet(Config* cfg);
+    ~ClientRadioSet(void);
+    int triggers(int i);
+    int radioRx(size_t radio_id, void* const* buffs, int numSamps,
         long long& frameTime);
-    int radioRx(size_t radio_id, size_t cell_id, void* const* buffs,
-        int numSamps, long long& frameTime);
-    void radioStart(void);
+    int radioTx(size_t radio_id, const void* const* buffs, int numSamps,
+        int flags, long long& frameTime);
     void radioStop(void);
     bool getRadioNotFound() { return radioNotFound; }
 
 private:
-    // use for create pthread
-    struct BaseRadioContext {
-        BaseRadioSet* brs;
+    struct ClientRadioContext {
+        ClientRadioSet* crs;
         std::atomic_ulong* thread_count;
         size_t tid;
-        size_t cell;
     };
-    void init(BaseRadioContext* context);
-    void configure(BaseRadioContext* context);
-
+    void init(ClientRadioContext* context);
     static void* init_launch(void* in_context);
-    static void* configure_launch(void* in_context);
-
-    void radioTrigger(void);
-    void sync_delays(size_t cellIdx);
-//    SoapySDR::Device* baseRadio(size_t cellId);
-    uhd::usrp::multi_usrp::sptr baseRadio(size_t cellId);
-    void collectCSI(bool&){};
-    void dciqCalibrationProc(size_t){};
-    void readSensors(void);
 
     Config* _cfg;
-//    std::vector<SoapySDR::Device*> hubs;
-    uhd::usrp::multi_usrp::sptr hubs;
-//    std::vector<std::vector<Radio*>> bsRadios; // [cell, iris]
-    Radio* bsRadios;
+//    std::vector<Radio*> radios;
+    Radio* radios;
     bool radioNotFound;
+
+//    SoapySDR::Device* check_dev;
 };
+
+#endif /* CLIENT_RADIO_SET_H */
