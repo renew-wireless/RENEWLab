* Enable choosing the beamforming method with demod option * Return SNR in samps2csi * Fix conjugate beamforming scaling

PYTHON/IrisUtils/channel_analysis.py
@@ -21,6 +21,7 @@ import math
 import time
 import datetime
 from scipy import signal
+import multiprocessing as mp
 import matplotlib.pyplot as plt
 from generate_sequence import *
 
@@ -364,12 +365,18 @@ def demult(csi, data, noise=None, method='zf'):
     """csi: Frame, User, Antenna, Subcarrier"""
     """data: Frame, Uplink Syms, Antenna, Subcarrier"""
     # Compute beamweights based on the specified frame.
+    demult_start = time.time()
     demul_data = np.empty(
         (csi.shape[0], data.shape[1], csi.shape[1], csi.shape[3]), dtype='complex64')
     bmf_w = np.empty(
         (csi.shape[0], csi.shape[3], csi.shape[2], csi.shape[1]), dtype='complex64')
     data_tp = np.transpose(data, (0, 3, 1, 2))
+    #pool = mp.Pool(mp.cpu_count())
+    #result_objects = [pool.apply_async(calcBW, args=(csi[i, :, :, :], noise[i, :, :, :] if noise is not None else None, method)) for i in range(csi.shape[0])]
+    #for i in range(csi.shape[0]):
+    #    bmf_w[i, :, :, :] = result_objects[i].get()[0]
     for frame in range(csi.shape[0]):
+        #bmf_w = calcBW(csi[frame], None if noise is None else noise[frame], method)
         for sc in range(csi.shape[3]):
             if method == 'zf':
                 bmf_w[frame, sc, :, :] = np.linalg.pinv(csi[frame, :, :, sc])
@@ -379,7 +386,30 @@ def demult(csi, data, noise=None, method='zf'):
                 w_mmse = np.matmul(np.linalg.inv(np.matmul(H, np.transpose(np.conj(H))) + sigma*np.eye(H.shape[0])), H)
                 bmf_w[frame, sc, :, :] = np.transpose(np.conj(w_mmse))
             else:
-                bmf_w[frame, sc, :, :] = np.transpose(
-                    np.conj(csi[frame, :, :, sc]), (1, 0))
+                csi_conj = np.conj(csi[frame, :, :, sc])
+                w_scale = 1 / np.sum(np.multiply(csi_conj, csi[frame, :, :, sc]), 1);
+                bmf_w[frame, sc, :, :] = np.transpose(np.matmul(np.diag(w_scale), csi_conj), (1, 0))
+                #bmf_w[frame, sc, :, :] = np.transpose(
+                #    np.conj(csi[frame, :, :, sc]), (1, 0))
     demul_data = np.transpose(np.matmul(data_tp, bmf_w), (0, 2, 3, 1))
+    print("demult time: %f"%(time.time() - demult_start))
     return demul_data
+
+def calcBW(csi, noise=None, method='zf'):
+    bmf_w = np.empty(
+        (csi.shape[2], csi.shape[1], csi.shape[0]), dtype='complex64')
+    for sc in range(csi.shape[2]):
+        if method == 'zf':
+            bmf_w[sc, :, :] = np.linalg.pinv(csi[:, :, sc])
+        elif method == 'mmse' and noise is not None:
+            sigma = np.mean(np.mean(np.power(np.abs(noise[:, :, sc]), 2), axis=0))
+            H = csi[:, :, sc]
+            w_mmse = np.matmul(np.linalg.inv(np.matmul(H, np.transpose(np.conj(H))) + sigma*np.eye(H.shape[0])), H)
+            bmf_w[sc, :, :] = np.transpose(np.conj(w_mmse))
+        else:
+            csi_conj = np.conj(csi[:, :, sc])
+            w_scale = np.sum(np.multiply(csi_conj, csi[:, :, sc]), 1);
+            bmf_w[sc, :, :] = np.transpose(np.diag(w_scale) * csi_conj, (1, 0))
+    return bmf_w
+
+

PYTHON/IrisUtils/hdf5_lib.py
@@ -500,23 +500,25 @@ class hdf5_lib:
                     #csi = np.memmap(os.path.join(_here,'temp1.mymemmap'), dtype='complex64', mode='w+', shape=(samps.shape[0], num_users, 2, samps.shape[1],52))
                     #iq = np.memmap(os.path.join(_here,'temp2.mymemmap'), dtype='complex64', mode='w+', shape=(samps.shape[0], num_users, 2, samps.shape[1],64))
             chunk_num = samps.shape[0]//chunk_size
-            csi0 = hdf5_lib.samps2csi(
+            csi0, SNR0 = hdf5_lib.samps2csi(
                     samps[:chunk_size, :, :, :], num_users, samps_per_user, fft_size, offset, bound, cp, pilot_f, legacy, 0)
             csi = np.empty(
                 (samps.shape[0], csi0.shape[1], csi0.shape[2], csi0.shape[3], csi0.shape[4]), dtype='complex64')
             csi[:chunk_size] = csi0
+            SNR = np.empty(
+                (samps.shape[0], csi0.shape[1], csi0.shape[2], csi0.shape[3]), dtype='complex64')
             for i in range(1, chunk_num):
-                csi[i*chunk_size:i*chunk_size+chunk_size] = hdf5_lib.samps2csi(
+                csi[i*chunk_size:i*chunk_size+chunk_size], SNR[i*chunk_size:i*chunk_size+chunk_size] = hdf5_lib.samps2csi(
                         samps[i*chunk_size:(i*chunk_size+chunk_size), :, :, :], num_users, samps_per_user, fft_size, offset, bound, cp, pilot_f, legacy, i)
             if samps.shape[0] > chunk_num*chunk_size:
-                csi[chunk_num*chunk_size:] = hdf5_lib.samps2csi(
+                csi[chunk_num*chunk_size:], SNR[chunk_num*chunk_size:] = hdf5_lib.samps2csi(
                     samps[chunk_num*chunk_size:, :, :, :], num_users, samps_per_user, fft_size, offset, bound, cp, pilot_f, legacy, chunk_num)
         else:
-            csi = hdf5_lib.samps2csi(
+            csi, SNR = hdf5_lib.samps2csi(
                 samps, num_users, samps_per_user, fft_size, offset, bound, cp, pilot_f, legacy)
 
         print("samps2csi_large took %f seconds" % (time.time() - samps2csi_large_start))
-        return csi
+        return csi, SNR
 
     @staticmethod
     def samps2csi(samps, num_users, samps_per_user=224, fft_size=64, offset=0, bound=94, cp=0, pilot_f=[], legacy=False, chunk_id=-1):
@@ -574,7 +576,15 @@ class hdf5_lib:
             csi = np.empty(iq.shape, dtype='complex64')
             # pre_csi = np.fft.fftshift(np.fft.fft(iq, fft_size, 4),4)
 
-            csi = np.fft.fftshift(np.fft.fft(iq, fft_size, 4), 4) * lts.lts_freq
+            iq_fft = np.fft.fftshift(np.fft.fft(iq, fft_size, 4), 4) 
+            csi = iq_fft * lts.lts_freq
+
+            zero_sc = [0, 1, 2, 3, 4, 5, 32, 59, 60, 61, 62, 63]
+            nonzero_sc = np.setdiff1d(range(64), zero_sc)
+            noise = iq_fft[:, :, :, :, zero_sc]
+            noise_power = np.mean(np.power(np.abs(noise), 2), 4) * len(nonzero_sc)
+            signal_power = np.sum(np.power(np.abs(iq_fft[:, :, :, :, nonzero_sc]), 2), 4)
+            SNR = (signal_power - noise_power) / noise_power
 
             # pyfftw.interfaces.cache.enable()
             # csi = pyfftw.interfaces.numpy_fft.fftshift(pyfftw.interfaces.numpy_fft.fft(iq, fft_size, 4),4)*lts.lts_freq
@@ -618,6 +628,14 @@ class hdf5_lib:
             iq_fft = np.fft.fft(iq, fft_size, 4)
             seq_freq_inv = 1 / pilot_f[nonzero_sc]
             csi = iq_fft[:, :, :, :, nonzero_sc] * seq_freq_inv
+            if len(zero_sc) == 0:
+                noise = None
+            else:
+                noise = iq_fft[:, :, :, :, zero_sc]
+                noise_power = np.mean(np.power(np.abs(noise), 2), 4) * len(nonzero_sc)
+                signal_power = np.sum(np.power(np.abs(iq_fft[:, :, :, :, nonzero_sc]), 2), 4)
+                SNR = signal_power / noise_power
+
             endtime = time.time()
             if debug:
                 print("csi.shape:{} lts_freq.shape: {}, pre_csi.shape = {}".format(
@@ -633,7 +651,7 @@ class hdf5_lib:
                 print("samps2csi chunk %d took %f seconds" % (chunk_id, time.time() - samps2csi_start))
             del iq
             gc.collect()
-        return csi
+        return csi, SNR
 
     @staticmethod
     def frame_sanity(match_filt, k_lts, n_lts, st_frame = 0, frame_to_plot = 0, plt_ant=0, cp=16):
@@ -812,7 +830,7 @@ class hdf5_lib:
         return snr, seq_found
 
     @staticmethod
-    def measure_cfo(pilot_samples, peak_map, pilot_type, pilot_seq, ofdm_len, z_padding, rate):
+    def measure_cfo(pilot_samples, pilot_type, pilot_seq, ofdm_len, z_padding, rate):
         n_frame = pilot_samples.shape[0]
         n_ue = pilot_samples.shape[1]
         n_ant = pilot_samples.shape[2]
@@ -829,9 +847,7 @@ class hdf5_lib:
                     IQ = I + (Q * 1j)
                     tx_pilot, lts_pks, lts_corr, pilot_thresh, best_pk = pilot_finder(IQ, pilot_type, flip=True,
                                                                                       pilot_seq=pilot_seq)
-                    print("frame %d ue %d ant %d lts_pks_len %d"%(frameIdx, ueIdx, bsAntIdx, lts_pks.size))
-                    print(lts_pks)
-                    if lts_pks.size < 2:
+                    if lts_pks.size != symbol_per_slot:
                         cfo[frameIdx, ueIdx, bsAntIdx] = 0
                     elif lts_pks[0] < ofdm_len or lts_pks[1] < ofdm_len:
                         cfo[frameIdx, ueIdx, bsAntIdx] = 0

PYTHON/IrisUtils/plot_hdf5.py
@@ -36,7 +36,7 @@ def verify_hdf5(hdf5, frame_i=100, cell_i=0, ofdm_sym_i=0, ant_i =0,
                 user_i=0, ul_slot_i=0, dl_slot_i=0, subcarrier_i=10, offset=-1,
                 dn_calib_offset=0, up_calib_offset=0, thresh=0.001,
                 deep_inspect=False, corr_thresh=0.00, exclude_bs_nodes=[],
-                demodulate=False, analyze=False):
+                demod="", analyze=False):
     """Plot data in the hdf5 file to verify contents.
 
     Args:
@@ -186,7 +186,7 @@ def verify_hdf5(hdf5, frame_i=100, cell_i=0, ofdm_sym_i=0, ant_i =0,
             # CSI:   #Frames, #Users, #Pilot Rep, #Antennas, #Subcarrier
             # For correlation use a fft size of 64
             print("*verify_hdf5(): Calling samps2csi with fft_size = {}, offset = {}, bound = {}, cp = {} *".format(fft_size, offset, z_padding, cp))
-            csi = hdf5_lib.samps2csi_large(pilot_samples, num_pilots, chunk_size, samps_per_slot, fft_size=fft_size,
+            csi,_ = hdf5_lib.samps2csi_large(pilot_samples, num_pilots, chunk_size, samps_per_slot, fft_size=fft_size,
                                             offset=offset, bound=z_padding, cp=cp, pilot_f=ofdm_pilot_f)
 
             if corr_thresh > 0.0:
@@ -211,7 +211,7 @@ def verify_hdf5(hdf5, frame_i=100, cell_i=0, ofdm_sym_i=0, ant_i =0,
             if analyze:
                 if noise_avail:
                     noise_samples = hdf5.noise_samples[:, cell_i, :, :, :]
-                    noise = hdf5_lib.samps2csi_large(noise_samples, noise_samples.shape[1], chunk_size, samps_per_slot, fft_size=fft_size,
+                    noise,_ = hdf5_lib.samps2csi_large(noise_samples, noise_samples.shape[1], chunk_size, samps_per_slot, fft_size=fft_size,
                                                 offset=offset, bound=z_padding, cp=cp, pilot_f=ofdm_pilot_f)
                     analyze_hdf5(csi, noise, metadata, ref_frame, subcarrier_i, offset)
                 else:
@@ -246,16 +246,16 @@ def verify_hdf5(hdf5, frame_i=100, cell_i=0, ofdm_sym_i=0, ant_i =0,
                 c_stop = min([(i+1)*chunk_size, calib_mat.shape[0]])
                 print("frames [%d, %d] "%(c_start, c_stop))
                 cal_samps = calib_pilot_samps[c_start:c_stop] 
-                csi_u = hdf5_lib.samps2csi_large(cal_samps[:, 1:2, :, :], 1, chunk_size, samps_per_slot, fft_size=fft_size,
+                csi_u, _ = hdf5_lib.samps2csi_large(cal_samps[:, 1:2, :, :], 1, chunk_size, samps_per_slot, fft_size=fft_size,
                                               offset=up_calib_offset, bound=z_padding, cp=cp, pilot_f=ofdm_pilot_f)
                 csi_u_one_sym = csi_u[:, 0, ofdm_sym_i, :, :]
-                csi_d = hdf5_lib.samps2csi_large(cal_samps[:, 0:1, :, :], 1, chunk_size, samps_per_slot, fft_size=fft_size,
+                csi_d, _ = hdf5_lib.samps2csi_large(cal_samps[:, 0:1, :, :], 1, chunk_size, samps_per_slot, fft_size=fft_size,
                                               offset=dn_calib_offset, bound=z_padding, cp=cp, pilot_f=ofdm_pilot_f)
                 csi_d_one_sym = csi_d[:, 0, ofdm_sym_i, :, :]
                 calib_mat[c_start:c_stop] = np.divide(csi_d_one_sym, csi_u_one_sym)
             plot_calib(calib_mat, calib_plot_bs_nodes, ref_frame, ant_i, subcarrier_i)
             if num_ues > 2: # actual clients are present
-                csi = hdf5_lib.samps2csi_large(calib_pilot_samps[:, 2:, :, :], num_cl, chunk_size, samps_per_slot, fft_size=fft_size,
+                csi, _ = hdf5_lib.samps2csi_large(calib_pilot_samps[:, 2:, :, :], num_cl, chunk_size, samps_per_slot, fft_size=fft_size,
                                                 offset=offset, bound=z_padding, cp=cp, pilot_f=ofdm_pilot_f)
                 uplink_csi = csi[:, :, ofdm_sym_i, :, :]
                 corr_total, sig_sc = calCorr(uplink_csi, np.transpose(np.conj(uplink_csi[ref_frame, :, :, :]), (1, 0, 2) ) )
@@ -310,16 +310,16 @@ def verify_hdf5(hdf5, frame_i=100, cell_i=0, ofdm_sym_i=0, ant_i =0,
         user_amps = np.mean(np.abs(ul_samps[:, :, ant_i, :]), axis=2)
         plot_iq_samps(ul_samps, user_amps, n_frm_st, ref_frame, [ul_slot_i], [ant_i], data_str="Uplink Data")
 
-        if demodulate:
-            tx_data = hdf5_lib.load_tx_data(metadata, hdf5.dirpath)
+        if demod=='zf' or demod=='conj' or demod=='mmse':
             if noise_avail:
                 noise_samples = hdf5.noise_samples[:, cell_i, :, :, :]
-                noise = hdf5_lib.samps2csi_large(noise_samples, noise_samples.shape[1], chunk_size, samps_per_slot, fft_size=fft_size,
+                noise, _ = hdf5_lib.samps2csi_large(noise_samples, noise_samples.shape[1], chunk_size, samps_per_slot, fft_size=fft_size,
                                             offset=offset, bound=z_padding, cp=cp, pilot_f=ofdm_pilot_f)
                 noise_f = noise[:, ul_slot_i, :, :, :]
-                equalized_symbols, tx_symbols, slot_evm, slot_evm_snr = hdf5_lib.demodulate(ul_samps[:, ul_slot_i, :, :], userCSI, tx_data, metadata, offset, ul_slot_i, noise_f, 'mmse')
             else:
-                equalized_symbols, tx_symbols, slot_evm, slot_evm_snr = hdf5_lib.demodulate(ul_samps[:, ul_slot_i, :, :], userCSI, tx_data, metadata, offset, ul_slot_i)
+                noise_f = None
+            tx_data = hdf5_lib.load_tx_data(metadata, hdf5.dirpath)
+            equalized_symbols, tx_symbols, slot_evm, slot_evm_snr = hdf5_lib.demodulate(ul_samps[:, ul_slot_i, :, :], userCSI, tx_data, metadata, offset, ul_slot_i, noise_f, demod)
             plot_constellation_stats(slot_evm, slot_evm_snr, equalized_symbols, tx_symbols, ref_frame, ul_slot_i)
 
     # Plot DL data symbols
@@ -443,7 +443,7 @@ def compute_legacy(hdf5):
         noise_meas_en = h5log.attrs.get('measured_noise', 1)
 
         # compute CSI for each user and get a nice numpy array
-        csi, iq = hdf5_lib.samps2csi(h5log['Pilot_Samples'], num_users + noise_meas_en, samps_per_user,
+        csi, SNR = hdf5_lib.samps2csi(h5log['Pilot_Samples'], num_users + noise_meas_en, samps_per_user,
                                      legacy=True)  # Returns csi with Frame, User, LTS (there are 2), BS ant, Subcarrier  #also, iq samples nicely chunked out, same dims, but subcarrier is sample.
         if zoom > 0:  # zoom in too look at behavior around peak (and reduce processing time)
             csi = csi[frame - zoom:frame + zoom, :, :, :, :]
@@ -519,7 +519,7 @@ def main():
     parser = OptionParser()
     parser.add_option("--show-metadata", action="store_true", dest="show_metadata", help="Displays hdf5 metadata", default=False)
     parser.add_option("--deep-inspect", action="store_true", dest="deep_inspect", help="Run script without analysis", default=False)
-    parser.add_option("--demodulate", action="store_true", dest="demodulate", help="Demodulate uplink data", default=False)
+    parser.add_option("--demodulate", type="string", dest="demodulate", help="Demodulate method for uplink data", default="")
     parser.add_option("--ref-frame", type="int", dest="ref_frame", help="Frame number to plot", default=1000)
     parser.add_option("--ref-ul-slot", type="int", dest="ref_ul_slot", help="UL slot number to plot", default=0)
     parser.add_option("--ref-dl-slot", type="int", dest="ref_dl_slot", help="DL slot number to plot", default=0)
@@ -582,7 +582,7 @@ def main():
         print("Setting the frame to inspect/plot to {}".format(fr_strt))
         ref_frame = 0
 
-    print(">> frame to plot = {}, ref. ant = {}, ref. user = {}, no. of frames to inspect = {}, starting frame = {} <<".format(ref_frame, ref_ant, ref_user, n_frames, fr_strt))
+    print(">> frame to plot = {}, ref. ant = {}, ref. user = {}, ref ofdm_sym = {}, no. of frames to inspect = {}, starting frame = {} <<".format(ref_frame, ref_ant, ref_user, ref_ofdm_sym, n_frames, fr_strt))
 
     # Instantiate
     if legacy:
@@ -628,8 +628,8 @@ def main():
         if show_metadata:
             print(hdf5.metadata)
         else:
-            if not ul_data_avail and demodulate:
-                demodulate = False
+            if not ul_data_avail and demodulate != "":
+                demodulate = ""
                 print("Uplink data is not available, ignoring demodulate option...")
 
             if verify: