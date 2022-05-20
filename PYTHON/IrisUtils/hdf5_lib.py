Bug fix SER calculation

PYTHON/IrisUtils/hdf5_lib.py
@@ -1044,7 +1044,9 @@ class hdf5_lib:
                 frame_end = frame_start + useful_frame_num
                 for i in range(frame_start, frame_end):
                     new_i = i - frame_start
-                    res = [k for k, l in zip(list(ul_demod_syms[i, j, :]), list(tx_data_syms[new_i, j, :])) if k == l]
+                    ul_demod_int = ofdm_obj.demodulation(ul_demod_syms[i, j, :], M)
+                    ul_tx_syms = ofdm_obj.demodulation(tx_data_syms[new_i, j, :], M)
+                    res = [k for k, l in zip(list(ul_demod_int), list(ul_tx_syms)) if k == l]
                     slot_ser[new_i, j] = (ul_demod_syms.shape[2] - len(list(res))) / ul_demod_syms.shape[2]
         else:
             # UL Syms: #Frames, #OFDM Symbols, #Antennas, #Samples
@@ -1066,17 +1068,18 @@ class hdf5_lib:
             ul_equal_syms = np.transpose(ul_equal_syms, (0, 2, 1, 3))
             # UL DATA: #Frames, #User, SLOT DATA SCs
             ul_equal_syms = np.reshape(ul_equal_syms, (ul_equal_syms.shape[0], ul_equal_syms.shape[1], symbol_per_slot * len(data_sc_ind)))
-            ul_demod_syms = np.empty(ul_equal_syms.shape, dtype="complex64")
+            ul_demod_syms = np.empty(ul_equal_syms.shape, dtype="int")
 
             for j in range(n_users):
                 frame_start = 0 if txdata.shape[0] == 1 else min_ue_offset
                 frame_end = frame_start + useful_frame_num
                 slot_evm[:, j] = np.linalg.norm(ul_equal_syms[frame_start:frame_end, j, :] - tx_data_syms[:useful_frame_num, j, :], 2, axis=1) / ul_equal_syms.shape[2]
                 for i in range(n_frames):
-                    ul_demod_syms[i, j, :] = ofdm_obj.modulation(ofdm_obj.demodulation(ul_equal_syms[i, j, :], M), M)
+                    ul_demod_syms[i, j, :] = ofdm_obj.demodulation(ul_equal_syms[i, j, :], M)
                 for i in range(frame_start, frame_end):
                     new_i = i - frame_start
-                    res = [k for k, l in zip(list(ul_demod_syms[i, j, :]), list(tx_data_syms[new_i, j, :])) if k == l]
+                    ul_tx_syms = ofdm_obj.demodulation(tx_data_syms[new_i, j, :], M)
+                    res = [k for k, l in zip(list(ul_demod_syms[i, j, :]), list(ul_tx_syms)) if k == l]
                     slot_ser[new_i, j] = (ul_demod_syms.shape[2] - len(list(res))) / ul_demod_syms.shape[2]
             slot_evm_snr = 10 * np.log10(1 / slot_evm)
 

PYTHON/IrisUtils/ofdmtxrx.py
@@ -134,11 +134,11 @@ class ofdmTxRx:
         RETURNS:
             - val: Integer value (data symbol) between 0 and 15 (i.e., 0:1:15)
         """
-
+        thresh = 2 / np.sqrt(10)
         val = (8 * (np.real(iq) > 0)) + \
-              (4 * (abs(np.real(iq)) < 0.6325)) + \
+              (4 * (abs(np.real(iq)) < thresh)) + \
               (2 * (np.imag(iq) > 0)) + \
-              (1 * (abs(np.imag(iq)) < 0.6325))
+              (1 * (abs(np.imag(iq)) < thresh))
         return val
 
     def qam64_dem(self, iq):
@@ -151,12 +151,15 @@ class ofdmTxRx:
         RETURNS:
             - val: Integer value (data symbol) between 0 and 63 (i.e., 0:1:63)
         """
+        thresh1 = 2 / np.sqrt(43)
+        thresh2 = 4 / np.sqrt(43)
+        thresh3 = 6 / np.sqrt(43)
         val = (32 * (np.real(iq) > 0)) + \
-              (16 * (abs(np.real(iq)) < 0.6172)) + \
-              (8 * ((abs(np.real(iq)) < 0.9258) and (abs(np.real(iq)) > 0.3086))) + \
+              (16 * (abs(np.real(iq)) < thresh2)) + \
+              (8 * ((abs(np.real(iq)) < thresh3) and (abs(np.real(iq)) > thresh1))) + \
               (4 * (np.imag(iq) > 0)) + \
-              (2 * (abs(np.imag(iq)) < 0.6172)) + \
-              (1 * ((abs(np.imag(iq)) < 0.9258) and (abs(np.imag(iq)) > 0.3086)))
+              (2 * (abs(np.imag(iq)) < thresh2)) + \
+              (1 * ((abs(np.imag(iq)) < thresh3) and (abs(np.imag(iq)) > thresh1)))
         return val
 
     def generate_data(self, n_ofdm_syms=100, mod_order=4, cp_length=16, datastream=[]):